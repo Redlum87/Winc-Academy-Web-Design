d47c3cdbb48df13a2fa70c311e818e4c117c8ade
Erwin <39443165+Redlum87@users.noreply.github.com>
2021-12-29 15:21:09 +0100

    Create script.js

---
+++ Opdracht_Function basics/script.js
@@ -0,0 +1,24 @@
+const makePizza = function(){
+    console.log("Make the dough");
+    console.log("Make pizza sauze");
+    console.log("Shape the dough");
+    console.log("Put the sause on the pizza shape");
+    console.log("Put ingredients on the pizza");
+    console.log("Bake the pizza in the oven");
+};
+
+// makePizza();
+// makePizza();
+// makePizza();
+
+const makeSushi = function(){
+    console.log("Cook rice");
+    console.log("Cut ingredients");
+    console.log("Rol rice and ingredients together");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
