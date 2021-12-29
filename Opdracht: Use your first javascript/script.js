524c0cd562d0bd6a4c6e38c9f829554ea1866456
Erwin <39443165+Redlum87@users.noreply.github.com>
2021-12-29 10:22:10 +0100

    Add files via upload

---
+++ Opdracht: Use your first javascript/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy");
+let name = "Erwin";
+console.log(name);
+console.log(4+4);
+console.log("4"+"4");
+console.log(4-4);
+console.log(4/4);
+console.log(4*4);
+console.log(4%4);
+
+let age = 34;
+console.log(typeof age);
+
+age = "vierendertig";
+
+console.log(typeof age);
