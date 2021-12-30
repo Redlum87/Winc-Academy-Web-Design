6b499df698380af14fb3bffea483a6b114246b2f
Erwin <39443165+Redlum87@users.noreply.github.com>
2021-12-30 11:19:01 +0100

    Create script.js

---
+++ Opdracht_Function_arguments/script.js
@@ -0,0 +1,9 @@
+const paintWalls = function(wall, color){
+    console.log("The "+wall+" has been painted "+color);
+    // console.log("");
+    // console.log("");
+}
+
+paintWalls("north wall", "orange");
+paintWalls("south-east wall", "green");
+paintWalls();
