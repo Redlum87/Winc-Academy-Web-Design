c86745d48abbc13d3ea5b649e301416b6ea0bcc9
Erwin <39443165+Redlum87@users.noreply.github.com>
2022-01-02 13:47:24 +0100

    Create multiplication-tables.js

---
+++ Opdracht_While_loops_&_for_loops/multiplication-tables.js
@@ -0,0 +1,12 @@
+// for (i=1 ; i <= 10; i++) {
+//     console.log(i + " x 9 = " + i*9)
+// }
+
+
+for(x=1; x <= 10; x++){
+    console.log("De tafel van " + x + ":")
+    for (i=1 ; i <= 10; i++) {
+        console.log(i + " x " + x + " = " + i*x)
+    }
+    
+}
