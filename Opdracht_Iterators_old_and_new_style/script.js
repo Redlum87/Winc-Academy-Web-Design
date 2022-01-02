192c98facf330d8a2270e9e391136d395f8782d8
Erwin <39443165+Redlum87@users.noreply.github.com>
2022-01-02 14:29:56 +0100

    Create script.js

---
+++ Opdracht_Iterators_old_and_new_style/script.js
@@ -0,0 +1,14 @@
+let colors = ["yellow", "blue", "red", "orange"]
+
+let x = 0
+while (x <  colors.length) {
+    console.log(colors[x])
+    x++
+}
+
+
+for (i = 0; i < colors.length; i++){
+console.log(colors[i])
+}
+
+colors.forEach(i => console.log(i))
